df3fd6f22c254488167b2ef0ee85722ce37c3134
datcanh <dotrananhduc@gmail.com>
2021-03-25 18:36:11 +0700

    Update thivien.py

--- nomscript/nomscript/spiders/thivien.py
+++ nomscript/nomscript/spiders/thivien.py
@@ -5,6 +5,8 @@ class ThivienSpider(scrapy.Spider):
     name = 'thivien'
     allowed_domains = ['https://www.thivien.net/']
     start_urls = ['https://www.thivien.net/searchpoem.php?Country=3']
+    custom_settings = { 'FEED_URI': "thivien.net_%(time)s.json",
+                       'FEED_FORMAT': 'json'}
 
     def parse(self, response):
         pass
