c720ffd74e514a29ea17d314a3dafae97f52e1de
datcanh <dotrananhduc@gmail.com>
2021-03-26 13:53:53 +0700

    Update vnthuquan.py
    - Parse ASP.Net site

--- nomscript/nomscript/spiders/vnthuquan.py
+++ nomscript/nomscript/spiders/vnthuquan.py
@@ -1,5 +1,5 @@
 import scrapy
-
+import js2xml
 
 class VnthuquanSpider(scrapy.Spider):
     name = 'vnthuquan'
@@ -13,12 +13,23 @@ class VnthuquanSpider(scrapy.Spider):
         print("URL: " + response.url)
         print("+===========+")
 
+        form_data = self.validate(response)
+
         post_urls = response.xpath('//li[@class="menutruyen"]//a/@href').extract()
         for url_item in post_urls:
-            url_merge = response.urljoin(url_item)
-            yield scrapy.Request(url_item, callback=self.parse_post)
+            # js2xml allows us to parse the JS function and params, and so to grab the __EVENTTARGET
+            js_xml  = js2xml.parse(url_item)
+            _id = js_xml.xpath("//identifier[@name='WebForm_PostBackOptions']/following-sibling::arguments/string[starts-with(.,'ctl')]")[0]
+            form_data["__EVENTTARGET"] = _id.text
 
-        # website not stable, will update later
+            url_diputado = response.urljoin(url_item)
+            # The proper way to send a POST in scrapy is by using the FormRequest
+            yield scrapy.FormRequest(url=url_diputado, formdata=form_data, callback=self.parse_post, method='POST')
+            
+
+            # url_merge = response.urljoin(url_item)
+            # yield scrapy.Request(url_merge, callback=self.parse_post)
+
         next_pages = response.xpath('//div/h1//a/@href').extract()
         for url_item in next_pages:
             yield scrapy.Request(url=response.urljoin(url_item), callback=self.parse)
@@ -38,3 +49,11 @@ class VnthuquanSpider(scrapy.Spider):
             'title': title,
             'text': ' '.join(response.xpath('//div[@class="truyen_text"]//text()').extract()),
         }
+
+    def validate(self, source):
+         # these fields are the minimum required as cannot be hardcoded
+        data = {"__VIEWSTATEGENERATOR": source.xpath("//*[@id='__VIEWSTATEGENERATOR']/@value")[0].extract(),
+                "__EVENTVALIDATION": source.xpath("//*[@id='__EVENTVALIDATION']/@value")[0].extract(),
+                "__VIEWSTATE": source.xpath("//*[@id='__VIEWSTATE']/@value")[0].extract(),
+                " __REQUESTDIGEST": source.xpath("//*[@id='__REQUESTDIGEST']/@value")[0].extract()}
+        return data
