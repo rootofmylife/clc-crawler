535bd3da1bc8244c0acbe954b067866da45d8b12
rootofmylife <dotrananhduc@gmail.com>
2021-05-16 16:05:17 +0700

    Create baohiem_vneconomy.py

---
+++ newspaper/newspaper/spiders/baohiem_vneconomy.py
@@ -0,0 +1,48 @@
+import scrapy
+
+
+class BaohiemVneconomySpider(scrapy.Spider):
+    name = 'baohiem_vneconomy'
+    start_urls = ['http://vneconomy.vn/bao-hiem-tai-chinh.htm']
+    custom_settings = { 'FEED_URI': "baohiem_vneconomy_%(time)s.json",
+                       'FEED_FORMAT': 'json',
+                       'FEED_EXPORT_ENCODING': 'utf-8'}
+
+    def parse(self, response):
+        print("+===========+")
+        print("URL: " + response.url)
+        print("+===========+")
+
+        count = 2
+
+        post_urls = response.xpath('//h3[@class="story__title"]//a/@href').extract()
+        for url_item in post_urls:
+            yield scrapy.Request('https://vneconomy.vn' + url_item, callback=self.parse_post)
+
+        while count < 2:
+            extent_url = "?trang=" + str(count) 
+            main_url = 'https://vneconomy.vn/bao-hiem-tai-chinh.htm' + extent_url
+            yield scrapy.Request(main_url, callback=self.parse)
+            count = count + 1
+
+    def parse_post(self, response):
+        print("Post URL: " + response.url)
+
+        if response.xpath('//div[@class="detail__author"]//strong//text()').get() == None:
+            author = ''
+        else:
+            author = response.xpath('//div[@class="detail__author"]//strong//text()').get().strip()
+
+        if response.xpath('//h2[@class="detail__summary"]//text()').get() == None:
+            sapo = ''
+        else:
+            sapo = response.xpath('//h2[@class="detail__summary"]//text()').get().strip()
+
+        yield {
+            'url': response.url,
+            'title': response.xpath('//h1[@class="detail__title"]/text()').get().strip(),
+            'author': author,
+            'date': response.xpath('//div[@class="detail__meta"]//text()').get().strip(),
+            'sapo': sapo,
+            'text': ' '.join(response.xpath('//div[@class="detail__content"]//text()').extract()).strip(),
+        }
